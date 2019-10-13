08198b264a9bfb8ba92d124d989dcc2299a8be93
Uriel <urielgarciarj@outlook.com>
2019-10-13 13:28:19 -0500

    Todos los botones necesarios fueron agregados al form de descargas

--- Practica1_SemSO/Descargas.Designer.cs
+++ Practica1_SemSO/Descargas.Designer.cs
@@ -28,12 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnDescargar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(70, 151);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(555, 23);
+            this.progressBar.TabIndex = 0;
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(70, 37);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(555, 20);
+            this.txtURL.TabIndex = 1;
+            // 
+            // btnDescargar
+            // 
+            this.btnDescargar.Location = new System.Drawing.Point(222, 76);
+            this.btnDescargar.Name = "btnDescargar";
+            this.btnDescargar.Size = new System.Drawing.Size(276, 45);
+            this.btnDescargar.TabIndex = 2;
+            this.btnDescargar.Text = "Descargar";
+            this.btnDescargar.UseVisualStyleBackColor = true;
+            // 
+            // Descargas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(698, 222);
+            this.Controls.Add(this.btnDescargar);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.progressBar);
+            this.Name = "Descargas";
             this.Text = "Descargas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnDescargar;
     }
 }
