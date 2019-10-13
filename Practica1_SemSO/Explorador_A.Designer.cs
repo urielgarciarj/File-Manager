6354ce1a944aeb9b8624b753c278ef0a2f382182
Uriel <urielgarciarj@outlook.com>
2019-10-13 15:44:37 -0500

    Formulario de Explorador_A agregado

---
+++ Practica1_SemSO/Explorador_A.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace Practica1_SemSO
+{
+    partial class Explorador_A
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(13, 24);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(362, 395);
+            this.treeView1.TabIndex = 0;
+            // 
+            // Explorador_A
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 428);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Explorador_A";
+            this.Text = "Explorador_A";
+            this.Load += new System.EventHandler(this.Explorador_A_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
---
+++ Practica1_SemSO/Explorador_A.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica1_SemSO
+{
+    public partial class Explorador_A : Form
+    {
+        public Explorador_A()
+        {
+            InitializeComponent();
+        }
+
+        private void Explorador_A_Load(object sender, EventArgs e)
+        {
+            string rutaBase = "D:";
+            
+            
+        }
+    }
+}
